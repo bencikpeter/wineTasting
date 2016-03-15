e6b169ff826d4f6d636a0d31be8bd0b5174d8202
Moroxus <lukas.tomovic@gmail.com>
2016-03-15 13:15:11 +0100

    updated enums WineColor and WineCharacter

--- src/main/java/cz/muni/fi/pv168/winetasting/backend/WineCharacter.java
+++ src/main/java/cz/muni/fi/pv168/winetasting/backend/WineCharacter.java
@@ -2,6 +2,8 @@ package cz.muni.fi.pv168.winetasting.backend;
 
 /**
  * Created by bencikpeter on 15.03.16.
+ * character of wine
  */
 public enum WineCharacter {
+    AKO, KAB, NZ, VZH, BV, HV, CV, LV, SV
 }
--- src/main/java/cz/muni/fi/pv168/winetasting/backend/WineColor.java
+++ src/main/java/cz/muni/fi/pv168/winetasting/backend/WineColor.java
@@ -2,6 +2,8 @@ package cz.muni.fi.pv168.winetasting.backend;
 
 /**
  * Created by bencikpeter on 15.03.16.
+ * color of wine
  */
 public enum WineColor {
+    WHITE, ROSE, RED
 }
