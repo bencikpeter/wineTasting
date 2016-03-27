3762775dafc8a80d6e62de6b8b599be9c24d0896
Moroxus <lukas.tomovic@gmail.com>
2016-03-27 15:55:55 +0200

    added method findSessionById to WineTastingDAO, created WineTastingManagerImpl and implemented some of its method

--- src/main/java/cz/muni/fi/pv168/winetasting/backend/WineTastingManager.java
+++ src/main/java/cz/muni/fi/pv168/winetasting/backend/WineTastingManager.java
@@ -6,10 +6,41 @@ import java.util.List;
  * Created by bencikpeter on 15.03.16.
  */
 public interface WineTastingManager {
+    /**
+     * Divides all wines assigned to specific session into several groups (commissions)
+     * these wines need to be in these groups for output at least, maybe in the database too
+     * @param session session, which will do the tasting and rating of wines
+     */
+    void generateWinesLayout(WineTastingSession session); // maybe should not return void, need to reconsider
 
-    public void generateWinesLayout(WineTastingSession session);
-    public void assignRatingToWine(WineSample sample);
-    public void assignWineToSession(WineTastingSession session, WineSample sample);
-    public WineTastingSession findSessionWithWine(WineSample sample);
-    public List<WineSample> findAllWinesInSession(WineTastingSession session);
+    /**
+     * assign wine sample to specific session
+     * this information needs to be stored in database table representing sample-session relation
+     * @param session wine sample assigned to session
+     * @param sample session, to which wine sample will be asigned
+     */
+    void assignWineToSession(WineTastingSession session, WineSample sample);
+
+    /**
+     * Specific session will rate specific wine sample with points
+     * this information needs to be stored in database table representing sample-session relation
+     * @param sample wine sample to be rated
+     * @param session session which will rate wine sample
+     * @param rating rating of sample, number between 0 to 100
+     */
+    void assignRatingToWine(WineSample sample, WineTastingSession session, int rating);
+
+    /**
+     * Finds session to which the wine sample is assigned
+     * @param sample specific wine sample
+     * @return session to which the wine sample is assigned
+     */
+    WineTastingSession findSessionWithWine(WineSample sample);
+
+    /**
+     * Finds all wines assigned to the session
+     * @param session specific session
+     * @return list of all wine samples assigned to the session
+     */
+    List<WineSample> findAllWinesInSession(WineTastingSession session);
 }
---
+++ src/main/java/cz/muni/fi/pv168/winetasting/backend/WineTastingManagerImpl.java
@@ -0,0 +1,145 @@
+package cz.muni.fi.pv168.winetasting.backend;
+
+import cz.muni.fi.pv168.winetasting.backend.Exceptions.ServiceFailureException;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lukas on 3/27/16.
+ */
+public class WineTastingManagerImpl implements WineTastingManager{
+
+    private DataSource dataSource;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public WineTastingManagerImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    private void checkDataSource(){
+        if (dataSource == null){
+            throw new IllegalStateException("DataSource is null");
+        }
+    }
+
+    @Override
+    public void generateWinesLayout(WineTastingSession session) {
+        //TODO implement algorithm to generateWinesLayout
+    }
+
+    @Override
+    public void assignWineToSession(WineTastingSession session, WineSample sample) {
+        checkDataSource();
+
+        if (session == null || sample == null) {
+            throw new IllegalArgumentException("at least one parameter is null");
+        }
+
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO WineTasting (sessionId, sampleId)" +
+                        "VALUES (?,?)")){
+            statement.setLong(1, session.getID());
+            statement.setLong(2, sample.getId());
+
+            int addedRows = statement.executeUpdate();
+            if (addedRows != 1) {
+                throw new ServiceFailureException("assignWineToSession: number of added rows is not one");
+            }
+        } catch (SQLException ex) {
+            throw new ServiceFailureException("error assigning wine to session",ex);
+        }
+
+    }
+
+    @Override
+    public void assignRatingToWine(WineSample sample, WineTastingSession session, int rating) {
+        checkDataSource();
+        if (rating < 0) {
+            throw new IllegalArgumentException("rating is below zero");
+        }
+        if (rating > 100) {
+            throw new IllegalArgumentException("rating is higher than 100");
+        }
+        if (session == null || sample == null) {
+            throw new IllegalArgumentException("at least one parameter is null");
+        }
+
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement("UPDATE WineTasting SET rating = ?" +
+                                                                        "WHERE sessionId = ? AND sampleId = ?")) {
+            statement.setInt(1, rating);
+            statement.setLong(2, session.getID());
+            statement.setLong(3, sample.getId());
+
+            int updatedRows = statement.executeUpdate();
+            if (updatedRows != 1) {
+                throw new ServiceFailureException("number of updated rows is not one");
+            }
+        } catch (SQLException ex) {
+            throw new ServiceFailureException("error assigning rating to wine", ex);
+        }
+    }
+
+    @Override
+    public WineTastingSession findSessionWithWine(WineSample sample) {
+        checkDataSource();
+        if (sample == null) {
+            throw new IllegalArgumentException("sample is null");
+        }
+
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement("SELECT sessionId FROM WineTasting " +
+                                                                        "WHERE sampleId = ?")) {
+            statement.setLong(1, sample.getId());
+            ResultSet resultSet = statement.executeQuery();
+            Long sessionId = null;
+            if (resultSet.next()) {
+                sessionId = resultSet.getLong("sessionId");
+                if (resultSet.next()) {
+                    throw new ServiceFailureException("retrieved more than one row from WineTasting");
+                }
+            }
+            WineTastingDAO manager = new WineTastingDAOImpl(dataSource);
+            WineTastingSession session = manager.findSessionById(sessionId);
+            return session;
+        } catch (SQLException ex) {
+            throw new ServiceFailureException("error finding session with specific wine", ex);
+        }
+    }
+
+    @Override
+    public List<WineSample> findAllWinesInSession(WineTastingSession session) {
+        checkDataSource();
+        if (session == null) {
+            throw new IllegalArgumentException("session is null");
+        }
+
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT  sampleId FROM WineTasting WHERE sessionId = ?")) {
+            statement.setLong(1, session.getID());
+            ResultSet resultSet = statement.executeQuery();
+            List<WineSample> wines = new ArrayList<>();
+            while (resultSet.next()) {
+                WineSampleDAO manager = new WineSampleDAOImpl(dataSource);
+                WineSample sample = manager.findWineSampleById(resultSet.getLong("id"));
+                if (sample != null) {
+                    wines.add(sample);
+                }
+            }
+            return wines;
+        } catch (SQLException ex) {
+            throw new ServiceFailureException("error finding all wines in specific session", ex);
+        }
+    }
+}
