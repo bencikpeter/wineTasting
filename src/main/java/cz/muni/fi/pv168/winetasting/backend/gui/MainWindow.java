9dc827e3cf38de4b045b00217aeb785712f7e673
Moroxus <lukas.tomovic@gmail.com>
2016-04-27 20:01:00 +0200

    gui form

---
+++ src/main/java/cz/muni/fi/pv168/winetasting/backend/gui/MainWindow.java
@@ -0,0 +1,25 @@
+package cz.muni.fi.pv168.winetasting.backend.gui;
+
+import javax.swing.*;
+
+/**
+ * Created by lukas on 4/27/16.
+ */
+public class MainWindow {
+    private JTabbedPane tabbedPane1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JSpinner spinner1;
+    private JButton button1;
+    private JTextField textField5;
+    private JComboBox comboBox3;
+    private JComboBox comboBox4;
+    private JComboBox comboBox5;
+    private JEditorPane ahojJaSomTvojeEditorPane;
+    private JButton button2;
+    private JButton button3;
+}
